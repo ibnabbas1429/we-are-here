bea9a82ea5c86ca3f961ae42a80db7e1d6e797a7
Aminu Ismail <63140904+ibnabbas1429@users.noreply.github.com>
2020-07-03 14:16:17 +0100

    new file add

---
+++ 5condition_exer7+favorite_pythoncrashcourse.py
@@ -0,0 +1,14 @@
+favourite_Fruits = ['Cucumber', 'Orange', 'Mango', 'Apple', 'Pawpaw', 'pineapple']
+
+avaliable_fruits = ['Mango', 'Apple','Pawpaw']
+
+for fruit in favourite_Fruits:
+    if fruit == 'Cucumber':
+        print('Whao! Cucumber is great')
+    if fruit == 'Orange':
+        print('Whao! Orange is great')
+    if fruit == 'Mango':
+        print('Whao! Mango is great')
+else: 
+        fruit != 'Mango'
+        print('Please make another choice')
